c1730d3a28657d2affb8bcec4ddcb131c2eeabe9
zhangzt <zhangzt@Ctrip.com>
2019-01-16 20:57:13 +0800

    Initial Project

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+require([
+    "hello"
+], function (hello) {
+    document.writeln("hello world!!");
+    document.writeln("<br/>");
+    // document.writeln(hello.name);
+    
+});
---
+++ scripts/this.js
@@ -0,0 +1,36 @@
+var obj = {
+    bar: 1,
+    foo: function () {
+        // window.document.write(this.bar);
+        // window.document.write("<br/>");
+        
+        window.document.body.append(this.bar);
+        window.document.body.append("<br/>");
+        // document.write(JSON.stringify(arguments.callee));
+        // document.write("<br/>");
+    }
+}
+
+var foo = obj.foo;
+var bar = 2;
+
+obj.foo();
+foo();
+
+
+var f = function () {
+    console.log(this);
+    console.log(this.x);
+  }
+  
+  var x = 1;
+  var obj = {
+    f: f,
+    x: 2,
+  };
+  
+  // 单独执行
+  f() // 1
+  
+  // obj 环境执行
+  obj.f() // 2
